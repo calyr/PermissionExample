bb67376cd9c7ade1800a7b85f8bb61b71be233c4
Roberto Carlos Callisaya Mamani <calyr.software@gmail.com>
2021-03-10 18:25:23 -0400

    added webWiew and INTERNET permission

--- app/src/main/java/app/bo/com/ucb/permissionexample/MainActivity.kt
+++ app/src/main/java/app/bo/com/ucb/permissionexample/MainActivity.kt
@@ -2,10 +2,15 @@ package app.bo.com.ucb.permissionexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val webView = WebView(applicationContext)
+        webView.settings.javaScriptEnabled = true
+        setContentView(webView)
+        webView.loadUrl("https://www.google.com")
     }
 }
